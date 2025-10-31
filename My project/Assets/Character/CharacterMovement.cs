30d27fd76b666a98aa88d431c6849fb58da842fd
Syyyck <brayandelimapf@gmail.com>
2025-10-30 21:27:50 -0300

    Movimentação e adicionando sprites

---
+++ My project/Assets/Character/CharacterMovement.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CharacterMovement : MonoBehaviour
+{
+    [SerializeField]private float moveSpeed = 5f;
+    private Rigidbody2D rb;
+    private Vector2 moveInput;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.linearVelocity = moveInput * moveSpeed;
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+    }
+}
